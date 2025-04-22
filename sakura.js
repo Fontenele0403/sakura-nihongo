775f08dcaa551f1b9a69e01cc8d1deb232f77d30
Fontenele0403 <pablosantosoff@gmail.com>
2025-04-21 21:53:34 -0300

    Add files via upload

---
+++ sakura.js
@@ -0,0 +1,56 @@
+const canvas = document.getElementById('sakura');
+const ctx = canvas.getContext('2d');
+let petals = [];
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Petal {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * -canvas.height;
+    this.radius = Math.random() * 4 + 2;
+    this.speedY = Math.random() * 1 + 0.5;
+    this.speedX = Math.random() * 0.5 - 0.25;
+    this.opacity = Math.random() * 0.5 + 0.3;
+  }
+
+  update() {
+    this.y += this.speedY;
+    this.x += this.speedX;
+    if (this.y > canvas.height) {
+      this.y = -10;
+      this.x = Math.random() * canvas.width;
+    }
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = `rgba(255, 182, 193, ${this.opacity})`;
+    ctx.fill();
+  }
+}
+
+function initPetals() {
+  for (let i = 0; i < 100; i++) {
+    petals.push(new Petal());
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  petals.forEach(petal => {
+    petal.update();
+    petal.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+initPetals();
+animate();
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
